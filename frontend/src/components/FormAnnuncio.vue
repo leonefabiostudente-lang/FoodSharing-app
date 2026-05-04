839039938ea338a0ecd8dc4f05b67c418343b452
Your Name <you@example.com>
2026-05-05 00:40:48 +0200

    Collegato inviaAnnuncio al backend Render

--- frontend/src/components/FormAnnuncio.vue
+++ frontend/src/components/FormAnnuncio.vue
@@ -26,7 +26,7 @@ async function inviaAnnuncio() {
     telefono_utente: telefono_utente.value
   };
 
-  const res = await fetch("http://localhost:5000/api/annunci", {
+ const res = await fetch("https://antispreco-app-2.onrender.com/api/annunci", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(nuovoAnnuncio)
