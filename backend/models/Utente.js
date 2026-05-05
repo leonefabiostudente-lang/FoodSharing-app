6059be40215d3d792617bbc6a298afc7f092fec2
Your Name <you@example.com>
2026-05-05 21:13:18 +0200

    Fix: hashing password, metodo compara_password, login funzionante

--- backend/models/Utente.js
+++ backend/models/Utente.js
@@ -1,88 +1,50 @@
 import mongoose from "mongoose";
-import bcryptjs from "bcryptjs"; // CORRETTO: importiamo bcryptjs con il nome giusto
+import bcryptjs from "bcryptjs";
 
 const utenteSchema = new mongoose.Schema(
   {
-    nome: {
-      type: String,
-      required: [true, 'Il nome è obbligatorio'],
-      trim: true,
-    },
+    nome: { type: String, required: true, trim: true },
     email: {
       type: String,
-      required: [true, 'L\'email è obbligatoria'],
+      required: true,
       unique: true,
       lowercase: true,
       match: [/.+\@.+\..+/, 'Email non valida'],
     },
     password: {
       type: String,
-      required: [true, 'La password è obbligatoria'],
+      required: true,
       minlength: 6,
-      select: false, // Non ritorna la password per default
+      select: false,
     },
-    telefono: {
-      type: String,
-      default: null,
-    },
+    telefono: String,
     tipo: {
       type: String,
       enum: ['privato', 'negozio', 'associazione'],
       default: 'privato',
     },
-    zona: {
-      type: String,
-      default: null,
-    },
-    indirizzo: {
-      type: String,
-      default: null,
-    },
-    foto_profilo: {
-      type: String,
-      default: null,
-    },
-    bio: {
-      type: String,
-      default: null,
-      maxlength: 500,
-    },
-    rating: {
-      type: Number,
-      default: 5,
-      min: 1,
-      max: 5,
-    },
-    numero_annunci: {
-      type: Number,
-      default: 0,
-    },
-    creato_il: {
-      type: Date,
-      default: Date.now,
-    },
+    zona: String,
+    indirizzo: String,
+    foto_profilo: String,
+    bio: { type: String, maxlength: 500 },
+    rating: { type: Number, default: 5, min: 1, max: 5 },
+    numero_annunci: { type: Number, default: 0 },
+    creato_il: { type: Date, default: Date.now },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-// Hash della password prima di salvare
+// Hash password prima del salvataggio
 utenteSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-
-  try {
-    const salt = await bcryptjs.genSalt(10);
-    this.password = await bcryptjs.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+  next();
 });
 
 // Metodo per confrontare password
-utenteSchema.methods.compara_password = async function (passwordInserita) {
-  return await bcryptjs.compare(passwordInserita, this.password);
+utenteSchema.methods.compara_password = function (passwordInserita) {
+  return bcryptjs.compare(passwordInserita, this.password);
 };
 
 const Utente = mongoose.model('Utente', utenteSchema);
