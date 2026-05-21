6b64444a2fded4be610597ee157ed04999247fbc
leonefabiostudente-lang <leone.fabio.studente@itispaleocapa.it>
2026-05-21 18:47:52 +0200

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://antispreco-app-2.onrender.com/api";
+
 const api = axios.create({
-  baseURL: "https://antispreco-app-2.onrender.com/api",
+  baseURL: API_BASE_URL,
   withCredentials: true
 });
 
