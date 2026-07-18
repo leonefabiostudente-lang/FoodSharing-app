678cbc46b6add77e7b2b23178d19fd1029ab57b3
Your Name <leone.fabio.studente@itispaleocapa.it>
2026-07-18 12:44:36 +0200

    chore: remove duplicated nested folders and add safe GA4 init

--- backend/frontend/android/app/src/main/java/it/fabioleone/foodsharing/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package it.fabioleone.foodsharing;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,23 @@ import 'bootswatch/dist/minty/bootstrap.min.css';
 import './assets/styles/global.css';
 import 'bootstrap';
 
+const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
+
+if (gaMeasurementId) {
+  const gaScript = document.createElement('script')
+  gaScript.async = true
+  gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`
+  document.head.appendChild(gaScript)
+
+  window.dataLayer = window.dataLayer || []
+  function gtag() {
+    window.dataLayer.push(arguments)
+  }
+
+  gtag('js', new Date())
+  gtag('config', gaMeasurementId)
+}
+
 createApp(App)
   .use(router)                  // <--- AGGIUNTO
   .use(i18n)                    // <--- AGGIUNTO
